b64689b0d82cb4d873f837922b7a942179d400c7
HarshulSharma000 <harshulsharma000@outlook.com>
2017-11-09 13:54:10 +0530

    Little changes

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,2 +1,3 @@
 export const zomatoKey = '25d1d57d18537d9db28d9794f33fe4fc';
 export const SHA1Key = '81:F6:E3:C7:3C:CC:43:B6:33:FE:72:B5:1F:22:B2:FB:72:1C:7C:D3';
+export const SHA256Key = 'E1:FF:95:51:E0:AF:85:7F:AE:D9:4C:41:E5:6A:D4:70:E3:C1:C2:35:E8:AC:FA:87:B7:B1:AB:B8:B6:A4:24:E4';
